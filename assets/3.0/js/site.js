9f508e525a7686656945eb8f41d1d7c5eb0b23c2
Neeraj Kumar Das <nkdas91@gmail.com>
2022-01-02 18:49:52 +0530

    Refactor layout

--- assets/3.0/js/site.js
+++ assets/3.0/js/site.js
@@ -1,113 +1,215 @@
-function getHighlightedText() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    return text;
-}
-
-$(function() {
-    $('.adspace').html(
-        `<a class="d-flex flex-column align-items-center" href="/" style="text-decoration:none;">
-            <img src="/assets/images/MSIconNewColorV2.svg" alt="Logo" style="width:150px; height:150px;">
-            <span class="text-dark text-center m-1">Material Style 2.0 is here!</span>
-        </a>`
-    );
-
-    $('.code-container').append('<span class="copy-btn"><b>click</b> or <b>highlight</b> to copy</span>');
-
-    $('.code-container').on('click', function(){
-        var clip = $("<textarea>");
-        $("body").append(clip);
-
-        var highlightedText = getHighlightedText();
-
-        if (highlightedText.length) {
-            clip.val(highlightedText.replace(/\u00a0/g, " ")).select();
-        } else {
-            clip.val($(this).find('code').find('br').prepend('\r\n').end().text().replace(/\u00a0/g, " ")).select();
-        }
-
-        document.execCommand("copy");
-        $(this).find('.copy-btn').html('<b>copied</b>');
-
-        $(this).find('code').find('br').text('');
-
-        clip.remove();
-    });
-
-    $('.code-container').on('mouseenter', function(){
-        $(this).find('.copy-btn').html('<b>click</b> or <b>highlight</b> to copy');
-    });
-
-    $('.this-page-links a').on('click', function(){
-        $('html, body').animate({scrollTop:$('#' + $(this).data('link')).offset().top - 120}, 500, 'swing');
-    });
-
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
-    scrollFunction();
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            $('#top-btn').css('display', 'inline-block');
-        } else {
-            $('#top-btn').css('display', 'none');
-        }
-    }
-
-    $('#top-btn').on('click', function(){
-        $('html, body').animate({scrollTop:0}, 500, 'swing');
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-    var active = false;
-
-    const lazyLoad = function() {
-        if (active === false) {
-            active = true;
-
-            setTimeout(function() {
-                lazyImages.forEach(function(lazyImage) {
-                    if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
-                        lazyImage.src = lazyImage.dataset.src;
-                        lazyImage.srcset = lazyImage.dataset.srcset;
-                        lazyImage.classList.remove("lazy");
-
-                        lazyImages = lazyImages.filter(function(image) {
-                            return image !== lazyImage;
-                        });
-
-                        if (lazyImages.length === 0) {
-                            document.removeEventListener("scroll", lazyLoad);
-                            window.removeEventListener("resize", lazyLoad);
-                            window.removeEventListener("orientationchange", lazyLoad);
-                        }
-                    }
-                });
-
-                active = false;
-            }, 200);
-        }
-    };
-
-    lazyLoad();
-    document.addEventListener("scroll", lazyLoad);
-    window.addEventListener("resize", lazyLoad);
-    window.addEventListener("orientationchange", lazyLoad);
-
-  // Remember offcanvas scroll position
+function saveOffcanvasScrollPosition() {
   let offcanvas = document.querySelector("#siteOffcanvas .offcanvas-body");
 
-  let positionTop = localStorage.getItem("offcanvas-scroll");
-  if (positionTop !== null) {
-    offcanvas.scrollTop = parseInt(positionTop, 10);
+  if (offcanvas) {
+    let positionTop = localStorage.getItem("offcanvas-scroll");
+    if (positionTop !== null) {
+      offcanvas.scrollTop = parseInt(positionTop, 10);
+    }
+
+    window.addEventListener("beforeunload", () => {
+      localStorage.setItem("offcanvas-scroll", offcanvas.scrollTop);
+    });
+  }
+}
+
+function lazyLoadImages() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  var active = false;
+
+  const lazyLoad = function () {
+    if (active === false) {
+      active = true;
+
+      setTimeout(function () {
+        lazyImages.forEach(function (lazyImage) {
+          if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+
+            lazyImages = lazyImages.filter(function (image) {
+              return image !== lazyImage;
+            });
+
+            if (lazyImages.length === 0) {
+              document.removeEventListener("scroll", lazyLoad);
+              window.removeEventListener("resize", lazyLoad);
+              window.removeEventListener("orientationchange", lazyLoad);
+            }
+          }
+        });
+
+        active = false;
+      }, 200);
+    }
+  };
+
+  lazyLoad();
+  document.addEventListener("scroll", lazyLoad);
+  window.addEventListener("resize", lazyLoad);
+  window.addEventListener("orientationchange", lazyLoad);
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    $('#top-btn').css('display', 'inline-block');
+  } else {
+    $('#top-btn').css('display', 'none');
+  }
+}
+
+function getHighlightedText() {
+  var text = "";
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
+  }
+  return text;
+}
+
+function initComponents() {
+  // Shape
+  var shapeList = [].slice.call(document.querySelectorAll('.m-shape-container'))
+  var shapes = shapeList.map(function (s) {
+    return new materialstyle.Shape(s)
+  })
+
+  // Button Ripple
+  var buttonList = [].slice.call(document.querySelectorAll('.btn'))
+  buttonList.map(function (button) {
+    new materialstyle.Ripple(button)
+  })
+
+  // Text Field
+  var textFieldList = [].slice.call(document.querySelectorAll('.m-text-field'))
+  var textFields = textFieldList.map(function (textField) {
+    return new materialstyle.TextField(textField)
+  })
+
+  // Select Field
+  var selectList = [].slice.call(document.querySelectorAll('.m-select'))
+  var selects = selectList.map(function (select) {
+    return new materialstyle.SelectField(select)
+  })
+
+  // Tab
+  var navLinks = [].slice.call(document.querySelectorAll('.nav-tabs .nav-link'))
+  navLinks.map(function (navLink) {
+    new materialstyle.Ripple(navLink)
+  })
+
+  var tabs = [].slice.call(document.querySelectorAll('.nav-tabs'))
+  tabs.map(function (tab) {
+    new materialstyle.Tab(tab)
+  })
+
+  // Popover
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new materialstyle.Popover(popoverTriggerEl)
+  })
+
+  // Progress Bar
+  var rainbowList = [].slice.call(document.querySelectorAll('.m-rainbow'))
+  var progressList = rainbowList.map(function (rainbow) {
+    return new materialstyle.Rainbow(rainbow)
+  })
+
+  // Spinner
+  var spinnerList = [].slice.call(document.querySelectorAll('.m-spinner'))
+  var spinners = spinnerList.map(function (s) {
+    return new materialstyle.Spinner(s)
+  })
+
+  // Modal
+  var myModal = document.getElementById('myModal')
+  if (myModal) {
+    myModal.addEventListener('shown.bs.modal', function (event) {
+      // Redraw Text Field
+      var textFields = document.querySelectorAll('.m-text-field');
+      for (const [, value] of Object.entries(textFields)) {
+        var textFieldInstance = materialstyle.TextField.getOrCreateInstance(value)
+        textFieldInstance.redraw();
+      }
+
+      // Redraw Select Field
+      var selectFields = document.querySelectorAll('.m-select');
+      for (const [, value] of Object.entries(selectFields)) {
+        var selectFieldInstance = materialstyle.SelectField.getOrCreateInstance(value)
+        selectFieldInstance.redraw();
+      }
+
+      // Redraw Tabs
+      var tabs = document.querySelector('.nav-tabs');
+      var tabInstance = materialstyle.Tab.getOrCreateInstance(tabs)
+      tabInstance.redraw();
+
+      // Snackbar
+      var snackbarButton = document.querySelector('#show-modal-snackbar');
+      snackbarButton.addEventListener('click', function () {
+        new materialstyle.Snackbar(document.querySelector('#modal-snackbar'), {
+          'visibleDuration': 3000
+        });
+      });
+
+    })
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  anchors.add('h2');
+
+  // Hide AdSpace
+  let ad = document.querySelector('.adspace');
+  if (ad) {
+    ad.innerHTML = '';
+    ad.style.display = 'none';
   }
 
-  window.addEventListener("beforeunload", () => {
-    localStorage.setItem("offcanvas-scroll", offcanvas.scrollTop);
+  // Hide / Show "Scroll to top" button
+  scrollFunction();
+  window.addEventListener('scroll', () => {
+    scrollFunction();
+  }, {
+    passive: true
+  })
+
+  lazyLoadImages()
+  saveOffcanvasScrollPosition()
+
+  initComponents()
+});
+
+$(function () {
+  $('.code-container').append('<span class="copy-btn"><b>click</b> or <b>highlight</b> to copy</span>');
+
+  $('.code-container').on('click', function () {
+    var clip = $("<textarea>");
+    $("body").append(clip);
+
+    var highlightedText = getHighlightedText();
+
+    if (highlightedText.length) {
+      clip.val(highlightedText.replace(/\u00a0/g, " ")).select();
+    } else {
+      clip.val($(this).find('code').find('br').prepend('\r\n').end().text().replace(/\u00a0/g, " ")).select();
+    }
+
+    document.execCommand("copy");
+    $(this).find('.copy-btn').html('<b>copied</b>');
+
+    $(this).find('code').find('br').text('');
+
+    clip.remove();
+  });
+
+  $('.code-container').on('mouseenter', function () {
+    $(this).find('.copy-btn').html('<b>click</b> or <b>highlight</b> to copy');
+  });
+
+  $('#top-btn').on('click', function () {
+    $('html, body').animate({scrollTop: 0}, 300, 'swing');
   });
 });
